7761906f9138078b05df2b1b11b5aa4a13e13451
Raptor-Rap <rmfikri42@gmail.com>
2024-05-01 20:34:36 +0700

    feat: Add login and splash page

---
+++ lib/screens/splash/splash_screen.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Timer(Duration(seconds: 3), () => Navigator.pushNamed(context, '/login'));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset(
+          'assets/images/splash.png',
+          width: 500,
+          height: 500,
+        ),
+      ),
+    );
+  }
+}
