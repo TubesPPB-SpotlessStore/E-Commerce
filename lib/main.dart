965531ed6c4fadb24c65ac6f7c1856deb857a53e
ZidhanArrasyid <arrasyidzidhan@gmail.com>
2024-06-12 13:34:40 +0700

    add detail

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:spotless_store/bloc/login/login_cubit.dart';
 import 'package:spotless_store/bloc/register/register_cubit.dart';
 import 'package:spotless_store/firebase_options.dart';
 //import 'package:spotless_store/bloc/login/login_cubit.dart';
-import 'package:spotless_store/screens/splash/splash_screen.dart';
 // import 'package:spotless_store/screens/splash/splash_screen.dart';
 import 'package:spotless_store/screens/splash.dart';
 import 'package:spotless_store/utils/routes.dart';
