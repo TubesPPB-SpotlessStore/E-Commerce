6b36ac50384cdd1e510afeea669a363154cdc6cd
Raptor-Rap <rmfikri42@gmail.com>
2024-06-12 14:22:15 +0700

    add splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:spotless_store/bloc/login/login_cubit.dart';
@@ -30,7 +31,7 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         navigatorKey: NAV_KEY,
         onGenerateRoute: generateRoute,
-        // home: SplashScreen(),
+        home: SplashScreen(),
       ),
     );
   }
